4ccc1c0ae4850f824bf0180fb08fc973e553f61b
Aniket Kumbhar <aniketkumbhar063@gmail.com>
2026-05-26 08:49:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+
+window.addEventListener("scroll", function () {
+
+  const header = document.querySelector(".header");
+
+  header.classList.toggle("sticky", window.scrollY > 100);
+
+});
